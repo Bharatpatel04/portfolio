77591d2f66c0cd77972c8bcdbc727d247c4d7935
Bharat Patel <bharat844081@gmail.com>
2023-07-16 04:40:48 +0530

    about section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,15 +15,11 @@ const About = () => {
             About
           </p>
           <p className="text-xl mt-20">
-            "Hello, my name is Bharat Patel and I am from Udaipur, Rajasthan,
-            India. I am a B.Tech student currently pursuing Computer Science at
-            PIET. With a solid academic background, having secured 87% and 92%
-            in my 12th and 10th exams respectively, I am deeply passionate about
-            web development. My experience and skills in this field include
-            proficiency in several programming languages and frameworks,
-            including HTML, CSS, JavaScript, and React. I have also completed
-            various web development projects, including a responsive website
-            that received recognition at a regional competition.
+          Hello, my name is Bharat Patel, and I am from Udaipur, Rajasthan, India. I am a B.Tech student currently pursuing Computer Science at PIET. With a solid academic background, having secured 87% and 92% in my 12th and 10th exams respectively, I am deeply passionate about web development.
+
+My experience and skills in this field include proficiency in several programming languages and frameworks, including HTML, CSS, JavaScript, and React. I have completed various web development projects, including a responsive website that received recognition at a regional competition.
+
+With a keen interest in technology, I am continuously exploring new avenues to expand my knowledge. Recently, I've been intrigued by the world of cloud infrastructure and its potential applications in the tech industry. As I embark on my journey in computer science, I am excited about the opportunities that lie ahead and the chance to contribute my skills to innovative projects. I pride myself on my ability to adapt quickly to new challenges and technologies. I am a determined problem-solver, always striving to find efficient solutions to complex issues.
           </p>
           <br />
           <p className="text-xl">
