d382b36f563f0e1ff23877500666821e1128d200
Bharat Patel <bharat844081@gmail.com>
2023-07-17 10:49:21 +0530

    about updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div
         className=" max-w-screen-lg p-4 mx-auto flex flex-col
@@ -14,7 +14,7 @@ const About = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
-          <p className="text-xl mt-20">
+          <p className="text-xl mt-20 h-auto">
           Hello, my name is Bharat Patel, and I am from Udaipur, Rajasthan, India. I am a B.Tech student currently pursuing Computer Science at PIET. With a solid academic background, having secured 87% and 92% in my 12th and 10th exams respectively, I am deeply passionate about web development.
 
 My experience and skills in this field include proficiency in several programming languages and frameworks, including HTML, CSS, JavaScript, and React. I have completed various web development projects, including a responsive website that received recognition at a regional competition.
